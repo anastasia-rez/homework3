96fe1bb8fc814e542d2b708df129cc6d7a92055f
anastasia-rez <nastya.reznichencko21@gmail.com>
2023-05-21 23:47:02 +0300

    склонения

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,3 @@
-// function text(){
-
-//     let first = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
-
-//     let second = ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
-
-//     let third = ['', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
-
-//     let numbers = sumUsers.value;
-//     console.log (numbers);
-
-//     let result = '';
-    
-
-//     for(let symbol of numbers) {
-//         if (numbers <= 9) {
-//             result = result + first[symbol];
-//         } 
-//     }
-
-//     for(let symbol of numbers) {
-//         if (numbers >= 10 && numbers <= 19) {
-//             result = second[symbol];
-//         }
-//     }
-
-//     for(let symbol of numbers) {
-//         if (numbers >= 20 && numbers <= 99) {
-//             result = third[symbol] + ' ' + first[symbol];
-//         }
-//     }
-
-
-
-//     numText.innerHTML = `${result}`;
-// }
-
-
 
 function text(){
 
@@ -45,7 +7,6 @@ function text(){
 
     let hundred = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
 
-    let valute = ['гривна', 'гривны', 'гривен']
 
     let numbers = sumUsers.value;
 
@@ -54,16 +15,27 @@ function text(){
     let result = '';
 
     if(numbers.length == 1 || numbers < 20){
-        result =  units[numbers] + ' ' + valute[2];
+        result =  units[numbers];
     }else if(numbers.length == 2) {
-        result = `${dozens[numbers[0]]} ${units[numbers[1]]}` + ' ' + valute[2];
+        result = `${dozens[numbers[0]]} ${units[numbers[1]]}`;
     } else if (numbers.length == 3) {
-        result = `${hundred[numbers[0]]} ${dozens[numbers[1]]} ${units[numbers[2]]}` + ' ' + valute[2];
+        result = `${hundred[numbers[0]]} ${dozens[numbers[1]]} ${units[numbers[2]]}`;
     }
 
+    console.log(result);
+
+
+    function declOfNum(number, titles) {
+        let cases = [2, 0, 1, 1, 1, 2];
+        return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+      }
+
+      numText.innerHTML = `${result} ${declOfNum(result, ['гривна', 'гривны', 'гривен'])}`;
     
 
 
+    // let declination = (number, txt, cases = [2, 0, 1, 1, 1, 2]) => txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 
-    numText.innerHTML = `${result}`;
+
+    // numText.innerHTML = `${result} ${declination(result, ['гривна', 'гривны', 'гривен'])}`;
 }
