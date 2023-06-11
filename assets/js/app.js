8b26d8623a2aa8a16448405550a87f7032d77a1a
anastasia-rez <nastya.reznichencko21@gmail.com>
2023-06-11 17:04:05 +0300

    numeric

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 
 function text(){
 
-    let units = ['', 'одна', 'две', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+    let units = ['', 'одна', 'две', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять'];
 
-    let dozens = ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+    let dozens =  ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто',];
 
     let hundred = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+
+    let numeric = ['', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
 
 
     let numbers = sumUsers.value;
@@ -16,18 +18,27 @@ function text(){
 
 
 
-    if(numbers.length == 1 || numbers < 20){
-        result =  units[numbers];
-    }else if(numbers.length == 2) {
+    if(numbers.lenght == 1 || numbers <= 10) {
+        result = units[numbers];
+    } else if (numbers.lenght == 2 || numbers < 20) {
+        result = numeric[numbers[1]];
+    } else if (numbers.lenght == 2 || numbers >= 20 ) {
         result = `${dozens[numbers[0]]} ${units[numbers[1]]}`;
-    } else if (numbers.length == 3) {
+    } 
+    
+    if (numbers.length == 3 && numbers[1] != '1') {
         result = `${hundred[numbers[0]]} ${dozens[numbers[1]]} ${units[numbers[2]]}`;
+    } else if (numbers.length == 3 && numbers[1] == '1'){
+        result  = `${hundred[numbers[0]]} ${numeric[numbers[2]]}`;
     }
+
+
 
     console.log(result);
 
-
     numbers = numbers.split('').reverse().join('');
+
+    console.log(numbers);
 
     if(numbers[0] == '1' && numbers[1] != '1'){
         numText.innerHTML = `${result} гривна`;
@@ -37,35 +48,4 @@ function text(){
         numText.innerHTML = `${result} гривен`;
     }
 
-
-    
-
-    // if (numbers % 10 == 1){
-    //     numText.innerHTML = `${result} гривна`;
-    // } else if (numbers % 10 == 2, 3, 4) {
-    //     numText.innerHTML = `${result} гривны`; 
-    // } else {
-    //     numText.innerHTML = `${result} гривен`;
-    // }
-
-
-
-
-
-
-
-    // function declOfNum(number, titles) {
-    //     let cases = [2, 0, 1, 1, 1, 2];
-    //     return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-    //   }
-
-    //   numText.innerHTML = `${result} ${declOfNum(result, ['гривна', 'гривны', 'гривен'])}`;
-    
-
-
-    // let declination = (number, txt, cases = [2, 0, 1, 1, 1, 2]) => txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-
-
-    // numText.innerHTML = `${result} ${declination(result, ['гривна', 'гривны', 'гривен'])}`;
-// }
 }
